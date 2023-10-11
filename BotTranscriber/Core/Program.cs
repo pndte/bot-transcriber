0a08a2dc26af18fc780e0e15e6afa67f0929350a
albert <baggerbig1@gmail.com>
2023-10-11 19:32:56 +0300

    Added VoiceMessage.cs

---
+++ BotTranscriber/Core/Program.cs
@@ -0,0 +1,24 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Vosk;
+using File = System.IO.File;
+
+Console.WriteLine("Hello, World!");
+
+
+var botClient = new TelegramBotClient($"{File.ReadAllText("C:\\Users\\Albert\\RiderProjects\\BotTranscriber\\BotTranscriber\\Core\\bot.config")}"); //TODO: заменить
+botClient.StartReceiving(UpdateHandler, ErrorHandler);
+
+async void UpdateHandler(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
+{
+    await Task.Run(() =>
+    {
+        
+    });
+}
+
+
+
+async void ErrorHandler(ITelegramBotClient bot, Exception exception, CancellationToken cancellationToken)
+{
+}
---
+++ BotTranscriber/Core/Voice/VoiceMessage.cs
@@ -0,0 +1,32 @@
+﻿using Vosk;
+
+namespace BotTranscriber.Voice;
+
+public class VoiceMessage
+{
+    private readonly string _path;
+
+    public VoiceMessage(string path)
+    {
+        _path = path;
+    }
+    
+    string Text(VoskRecognizer recognizer)
+    {
+        //var vosk = new VoskRecognizer(new Model("C:\\Users\\Albert\\Desktop\\vosk-model-small-ru-0.22"), 48000);
+        //vosk.SetMaxAlternatives(0);
+        //vosk.SetWords(true);
+        using (Stream source = File.OpenRead(_path))
+        {
+            byte[] buffer = new byte[16384];
+            int bytesRead;
+            while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                Console.WriteLine(recognizer.AcceptWaveform(buffer, bytesRead) ? recognizer.Result() : recognizer.PartialResult());
+            }
+        }
+
+        Console.WriteLine($"Результат: {recognizer.FinalResult()}");
+        return recognizer.FinalResult();
+    }
+}
